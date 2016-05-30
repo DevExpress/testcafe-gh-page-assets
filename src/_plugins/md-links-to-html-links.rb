41d30c4a3471e8f34080b4ca845572d8681b2f1b
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2016-05-30 15:49:13 +0300

    Fix processing links to index pages in the current dir (#5)

--- src/_plugins/md-links-to-html-links.rb
+++ src/_plugins/md-links-to-html-links.rb
@@ -1,11 +1,16 @@
 module ConvertMdLinksToHtml
     class Generator < Jekyll::Generator
+        LINK_TO_OTHER_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*\/)(index\.md)(#[^\)]*)?\)/
+        LINK_TO_CURRENT_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*)((?<=\()index\.md)(#[^\)]*)?\)/
+        NON_INDEX_LINK_RE = /(\[[^\]]*\]\([^:\)]*)(?<!\/index)(?<!\(index)\.md(#[^\)]*)?\)/
+
         def generate(site)
             site.pages.each { |p| convert_links(site, p) }
         end
         def convert_links(site, page)
-            page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)(index\.md)(#[^\)]*)?\)/, '\1\3)')
-            page.content = page.content.gsub(/(\[[^\]]*\]\([^:\)]*)\.md(#[^\)]*)?\)/, '\1.html\2)')
+            page.content = page.content.gsub(LINK_TO_OTHER_DIR_INDEX_RE, '\1\3)')
+            page.content = page.content.gsub(LINK_TO_CURRENT_DIR_INDEX_RE, '\1.\3)')
+            page.content = page.content.gsub(NON_INDEX_LINK_RE, '\1.html\2)')
         end
     end
 end
