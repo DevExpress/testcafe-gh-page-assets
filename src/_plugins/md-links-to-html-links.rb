e652093728e155e645c6286e999dfaa11c811229
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2017-03-09 10:17:34 +0300

    rename index.md to readme.md (#32)

--- src/_plugins/md-links-to-html-links.rb
+++ src/_plugins/md-links-to-html-links.rb
@@ -1,8 +1,8 @@
 module ConvertMdLinksToHtml
     class Generator < Jekyll::Generator
-        LINK_TO_OTHER_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*\/)(index\.md)(#[^\)]*)?\)/
-        LINK_TO_CURRENT_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*)((?<=\()index\.md)(#[^\)]*)?\)/
-        NON_INDEX_LINK_RE = /(\[[^\]]*\]\([^:\)]*)(?<!\/index)(?<!\(index)\.md(#[^\)]*)?\)/
+        LINK_TO_OTHER_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*\/)(README\.md)(#[^\)]*)?\)/
+        LINK_TO_CURRENT_DIR_INDEX_RE = /(\[[^\]]*\]\([^:\)]*)((?<=\()README\.md)(#[^\)]*)?\)/
+        NON_INDEX_LINK_RE = /(\[[^\]]*\]\([^:\)]*)(?<!\/README)(?<!\(README)\.md(#[^\)]*)?\)/
 
         def generate(site)
             site.pages.each { |p| convert_links(site, p) }
