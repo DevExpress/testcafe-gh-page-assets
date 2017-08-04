6c3e3f68ed033b3e517c621dcb0d777e62d8c6e0
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2017-08-04 14:38:26 +0300

    Add a widget (#34)
    * Add a widget
    
    * Reduce time between visits
    
    * Choose more appropriate names for analytic events
    
    * Address Alexander's remarks
    
    * Update the tweet text
    
    * Return to old logic for displaying widget
    
    * Add logic to send widget's first open to GA
    
    * Close the widget on mousedown and touchstart
    
    * Make the widget panel background darker

---
+++ src/scripts/widget.js
@@ -0,0 +1,67 @@
+---
+---
+$(function() {
+    var WIDGET_DISPLAYED_FLAG = 'widget-displayed';
+    var WIDGET_OPENED_FLAG = 'widget-opened';
+
+    function sendGaEvent(event) {
+        {% if jekyll.environment == "production" %}
+
+        ga('send', 'event', 'shareWidget', event);
+
+        {% endif %}
+    }
+
+    function updateWidgetVisibility() {
+        if(window.needShowShareWidget()) {
+            $('.widget-container').removeClass('hidden');
+            if(!window.localStorage.getItem(WIDGET_DISPLAYED_FLAG)) {
+                sendGaEvent('shareWidgetFirstDisplay');
+                window.localStorage.setItem(WIDGET_DISPLAYED_FLAG, true);
+            }
+        }
+        else 
+            $('.widget-container').addClass('hidden');
+    }
+
+    function handleWidgetAction(event) {
+        updateWidgetVisibility();
+        sendGaEvent(event);
+    }
+
+    function openWidget() {
+        $('.widget-container').addClass('out');
+        
+        if(!window.localStorage.getItem(WIDGET_OPENED_FLAG)) {
+            sendGaEvent('shareWidgetFirstOpen');
+            window.localStorage.setItem(WIDGET_OPENED_FLAG, true);
+        }
+    }
+
+    $('#share-button').on('click', function () {
+        openWidget();
+    });
+
+    $('#tweet').on('click', function () {
+        handleWidgetAction('shareWidgetTwitter');
+    });
+
+    $('#facebook').on('click', function () {
+        handleWidgetAction('shareWidgetFacebook');
+    });
+    
+    $('#collapse').on('click', function () {
+        $('.widget-container').removeClass('out');
+    });
+
+    $(document).bind( "mousedown touchstart", function(e) {
+        var widget = $('.widget-container');
+
+        var clickOutsideWidget = !widget.is(e.target) && widget.has(e.target).length === 0;
+
+        if (clickOutsideWidget) 
+            widget.removeClass('out');
+    });
+
+    updateWidgetVisibility();
+});
