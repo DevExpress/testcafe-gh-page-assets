bd1e0c195673ab84f956bf4daf5d62ac16d2ce68
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2018-03-16 15:28:25 +0300

    Introduce the tweet gallery (#50)

---
+++ src/scripts/tweet-gallery.js
@@ -0,0 +1,35 @@
+---
+---
+$(function() {
+    var tweets = [
+        {% for tweet in site.data.tweets %}
+            {
+                url: '{{ tweet.url }}'
+            }{% if forloop.last == false %},{% endif %}
+        {% endfor %}
+    ];
+
+    var md      = new MobileDetect(window.navigator.userAgent);
+    var showNav = !md.mobile();
+
+    $('#tweet-gallery').dxGallery({
+        dataSource: tweets,
+        width: "100%",
+        height: 300,
+        showIndicator: false,
+        showNavButtons: showNav,
+        initialItemWidth: 400,
+        loop: true,
+        itemTemplate: function (item, index) {
+            var result = $("<div>");
+
+            $.ajax({
+                url: "https://api.twitter.com/1/statuses/oembed.json?hide_thread=true&hide_media=true&url=" + item.url,
+                dataType: "jsonp",
+                success: function (data) { result.html(data.html); }
+            });
+
+            return result;
+        }
+    });
+})
