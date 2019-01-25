84c00e7d0f564d2047e692f28b0f543be9c21e2e
VasilyStrelyaev <bazilstrel@gmail.com>
2019-01-25 17:49:45 +0300

    Switch from GIFs to videos

---
+++ src/_plugins/videos.rb
@@ -0,0 +1,14 @@
+require 'redcarpet'
+
+class Redcarpet::Render::HTML
+    def image(link, title, alt_text)
+        if link.include? ".gif"
+            return "<video autoplay loop muted playsinline>"\
+                   "    <source src=\"#{link.sub(".gif", ".webm")}\" type=\"video/webm\">"\
+                   "    <source src=\"#{link.sub(".gif", ".mp4")}\" type=\"video/mp4\">"\
+                   "</video>"
+        else
+            return "<img src=\"#{link}\" alt=\"#{alt_text}\" title=\"#{title}\">"
+        end
+    end
+end
